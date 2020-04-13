61c51fb457b01427df268eb9db50d3b0031d8bb3
Fabienne <fabienne@fidzup.com>
2020-04-13 20:06:22 +0200

    change version

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,7 +2,7 @@ require "rulers/version"
 require "rulers/array"
 
 module Rulers
-  class Error < StandardError; end
+  # class Error < StandardError; end
   class Application
     def call(env)
       [200, { 'Content-Type'=> 'text/html' },
