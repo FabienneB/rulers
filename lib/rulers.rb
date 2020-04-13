f24b04840222ae8c74f416394527127d5deaa24b
Fabienne <fabienne@fidzup.com>
2020-04-13 20:25:15 +0200

    add debugger

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,9 +2,10 @@ require "rulers/version"
 require "rulers/array"
 
 module Rulers
-  # class Error < StandardError; end
+  class Error < StandardError; end
   class Application
     def call(env)
+      `echo debug > debug.txt`
       [200, { 'Content-Type'=> 'text/html' },
         ["Hello from ruby on rulers"]]
     end
