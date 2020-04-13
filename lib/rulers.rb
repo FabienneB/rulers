2279e6fd4e24e848a38d65de40920a7362d4ccc4
Fabienne <fabienne@fidzup.com>
2020-04-13 16:58:05 +0200

    new gem version

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,10 +1,11 @@
 require "rulers/version"
+require "rulers/array"
 
 module Rulers
   class Error < StandardError; end
   class Application
     def call(env)
-      [200, { 'Content-Type'=> 'test/html' },
+      [200, { 'Content-Type'=> 'text/html' },
         ["Hello from ruby on rulers"]]
     end
   end
